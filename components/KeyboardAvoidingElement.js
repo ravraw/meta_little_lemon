718653e47e9568eee8846681559e1417417dfd09
Ravindra Rawat <ravinrawat.wd@gmail.com>
2024-05-01 18:47:41 -0600

    Added KeyboardAvoidingElement

---
+++ components/KeyboardAvoidingElement.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { KeyboardAvoidingView, StyleSheet, Platform } from "react-native";
+
+const KeyboardAvoidingElement = ({ children }) => {
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default KeyboardAvoidingElement;
