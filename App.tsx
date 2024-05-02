ab47cd3c4dbf791ef9706777abfbab89674a44a8
Ravindra Rawat <ravinrawat.wd@gmail.com>
2024-05-01 18:50:24 -0600

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./navigators/RootNavigator";
+import { Text } from "react-native-paper";
 
 export default function App() {
   return (
