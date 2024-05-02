d222eb7216bed189542cd74dc72b73f79b155008
Ravindra Rawat <ravinrawat.wd@gmail.com>
2024-05-01 18:49:54 -0600

    Updated Onboarding screen

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,16 +1,127 @@
-import React from "react";
-import { Pressable, TextInput, View } from "react-native"
+import React, { useState } from 'react'
+import {
+    Text,
+    TextInput,
+    View,
+    Image,
+    StyleSheet,
+    Pressable,
+    Alert,
+} from 'react-native'
+import KeyboardAvoidingElement from '../components/KeyboardAvoidingElement'
+import { validateEmail } from '../utils'
+import Header from '../components/Header'
 
+const Onboarding = ({ navigation }) => {
+    const [email, setEmail] = useState('')
+    const [isValid, setIsValid] = useState(false)
 
-const Onboarding = () => {
-    return(
-        <View>
-            <Header/>
-            <TextInput/>
-            <TextInput/>
-            <Pressable></Pressable>
-        </View>
+    const isValidEmail = () => {
+        setIsValid(validateEmail(email))
+    }
+    const showAlert = () =>
+        Alert.alert('', 'Thanks for subscribing, stay tuned!', [
+            { text: 'OK', onPress: () => navigation.navigate('Welcome') },
+        ])
+
+    return (
+        <KeyboardAvoidingElement>
+            <View style={styles.container}>
+                <Header />
+                <Text style={styles.subheading}>Lets us get to know you</Text>
+                <View style={styles.inputContainer}>
+                    <View style={styles.textInputWrapper}>
+                        <Text style={styles.textInputLabel}>First Name</Text>
+                        <TextInput
+                            value={email}
+                            placeholder="Hello@example.com"
+                            onChangeText={setEmail}
+                            onChange={isValidEmail}
+                            keyboardType="email-address"
+                            maxLength={50}
+                            style={styles.textInput}
+                            clearButtonMode="always"
+                        />
+                    </View>
+                    <View style={styles.textInputWrapper}>
+                        <Text style={styles.textInputLabel}>Email</Text>
+                        <TextInput
+                            value={email}
+                            placeholder="Hello@example.com"
+                            onChangeText={setEmail}
+                            onChange={isValidEmail}
+                            keyboardType="email-address"
+                            maxLength={50}
+                            style={styles.textInput}
+                            clearButtonMode="always"
+                        />
+                    </View>
+                </View>
+
+                <Pressable
+                    style={[
+                        styles.button,
+                        !isValid && { backgroundColor: 'gray' },
+                    ]}
+                    disabled={!isValid}
+                    onPress={showAlert}
+                >
+                    <Text style={styles.buttonText}>Next</Text>
+                </Pressable>
+            </View>
+        </KeyboardAvoidingElement>
     )
 }
 
-export default Onboarding;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    subheading: {
+        width: '90%',
+        justifyContent: 'center',
+        alignContent: 'center',
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    inputContainer: {
+        width: '100%',
+        alignItems: 'center',
+    },
+    textInputWrapper: {
+        width: '80%',
+        alignItems: 'center',
+        marginVertical: 15,
+    },
+    textInputLabel: {
+        alignSelf: 'flex-start',
+        marginVertical: 5,
+    },
+    textInput: {
+        height: 50,
+        width: '100%',
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: 'black',
+    },
+    button: {
+        height: 40,
+        width: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '10%',
+        marginBottom: 25,
+        alignSelf: 'flex-end',
+        borderRadius: 10,
+        backgroundColor: '#495E57',
+    },
+    buttonText: {
+        color: '#FFFFFF',
+    },
+})
+
+export default Onboarding
