18076ca53d238ca11b346a9f15505d8cc1c206be
Ravindra Rawat <ravinrawat.wd@gmail.com>
2024-05-05 16:07:54 -0600

    Updated styles for menu items

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -5,15 +5,19 @@ const MenuItem = ({ name, description, price, image }) => {
     console.log(`${name}`)
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>{name}</Text>
-            <Text style={styles.des}>{description}</Text>
-            <Text style={styles.text}>{price}</Text>
-            <Image
-                style={styles.avatarImage}
-                source={{
-                    uri: 'https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${image}?raw=true',
-                }}
-            />
+            <View style={styles.contentLeft}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.description}>{description}</Text>
+                <Text style={styles.price}>{`$${price}`}</Text>
+            </View>
+            <View style={styles.contentRight}>
+                <Image
+                    style={styles.itemImage}
+                    source={{
+                        uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${image}?raw=true`,
+                    }}
+                />
+            </View>
         </View>
     )
 }
@@ -23,12 +27,39 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignSelf: 'center',
-        paddingVertical: 15,
-        paddingHorizontal: 25,
-        marginVertical: 10,
+        padding: 5,
+        marginVertical: 5,
         backgroundColor: '#fff',
         borderRadius: 10,
         width: '100%',
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+    },
+    name: {
+        fontSize: 18,
+        fontFamily: 'Karla',
+        fontWeight: 'bold',
+    },
+    description: {
+        fontSize: 14,
+        fontFamily: 'Karla',
+        fontWeight: '300',
+        paddingVertical: 5,
+    },
+    price: {
+        fontSize: 16,
+        fontFamily: 'Karla',
+        fontWeight: 'bold',
+    },
+    itemImage: {
+        width: 90,
+        height: 90,
+    },
+    contentLeft: {
+        width: '70%',
+    },
+    contentRight: {
+        justifyContent: 'center',
     },
 })
 
