66166e3e07d5dd91bd8abe7ed6cc738f7475bf45
Ravindra Rawat <ravinrawat.wd@gmail.com>
2024-05-11 12:52:52 -0600

    Added useEffect for updates only

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,29 @@
 import { useRef, useEffect } from 'react'
 
-export const capitalize = (word) => {
+export const capitalize = (word = 'start') => {
     return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+export function useUpdateEffect(effect, dependencies = []) {
+    const isInitialMount = useRef(true)
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false
+        } else {
+            return effect()
+        }
+    }, dependencies)
+}
+
+export const debounce = (func, delay) => {
+    let timeoutId
+
+    return (...args) => {
+        clearTimeout(timeoutId)
+
+        timeoutId = setTimeout(() => {
+            func.apply(this, args)
+        }, delay)
+    }
 }
